e1d1384c9058a01566aa41f7586f604360fc3123
Tia H <tfharris@student.fullsail.edu>
2021-02-14 08:44:52 -0700

    Finished the option to add a fulltime employee in the application class.

--- Week2/Tykeeja_Harris_CE03/Tykeeja_Harris_CE03/Application.cs
+++ Week2/Tykeeja_Harris_CE03/Tykeeja_Harris_CE03/Application.cs
@@ -12,7 +12,7 @@ namespace Tykeeja_Harris_CE03
 
         public Application()
         {
-           
+
 
             //Add employees from text file
             //define a string for the file location of the text file
@@ -40,16 +40,14 @@ namespace Tykeeja_Harris_CE03
                         decimal hpw = decimal.Parse(data[2]);
 
                         decimal pph = decimal.Parse(data[3]);
-                        fulltime  = new FullTime(data[0], data[1], pph, hpw);
+                        fulltime = new FullTime(data[0], data[1], pph, hpw);
                         //add fulltime employee to list
                         employees.Add(fulltime);
 
-                       
-                        
 
                     }
 
-                  
+
                 }
 
             }
@@ -66,14 +64,14 @@ namespace Tykeeja_Harris_CE03
             }
 
             //instantiate a dynamic menu of movie objects
-            List<string> userList = new List<string>() { "Add Employee", "Remove Employee", "Display Payroll", "Exit"};
+            List<string> userList = new List<string>() { "Add Employee", "Remove Employee", "Display Payroll", "Exit" };
             menu user = new menu(userList);
 
             bool display = true;
             while (display)
             {
                 //Display title
-                UI.ActionText("\r\nEmployee Tracker");
+                UI.ActionText("\r\nAdd Employee");
                 UI.Seperator();
                 //reset the color
                 Console.ResetColor();
@@ -111,7 +109,7 @@ namespace Tykeeja_Harris_CE03
                     case 4:
                         display = false;
                         break;
-                      
+
 
                 }
             }
@@ -121,12 +119,164 @@ namespace Tykeeja_Harris_CE03
 
         }
 
-        
 
+        //create a method that allows you to add empployees
         public void AddEmployee()
         {
 
-        }
+            List<string> userList = new List<string>() { "FullTime", "PartTime", "Salaried", "Manager" };
+            menu user = new menu(userList);
+
+            bool display = true;
+            while (display)
+            {
+                //Display title
+                UI.ActionText("\r\nEmployee Tracker");
+                UI.Seperator();
+                //reset the color
+                Console.ResetColor();
+
+                //Instantiate
+                Console.WriteLine("\r\n");
+                user.DisplayMethod();
+                Console.WriteLine("\r\n");
+
+                //ask the user for the selection
+                Console.WriteLine("Please enter your selection:");
+                //catch the user repsonse
+                string response = Console.ReadLine();
+                //make sure the response is not blank
+                Validation.StringValidation(response);
+
+
+              //  Begin switch statement that runs the option the user selected
+                        switch (response.ToLower())
+
+                {
+                    case "fulltime":
+                    case "1":
+                        Console.Clear();
+                        //ask for the employees name
+                        Console.WriteLine("What is the employee's name?");
+                        //catch the employees name
+                        string empName = Console.ReadLine();
+                        //validate the user entered valid input
+                        Validation.StringValidation(empName);
+                        //ask the employees address
+                        Console.WriteLine("What is the employees address?");
+                        //catch the user response
+                        string empAdd = Console.ReadLine();
+                        //validate the user entered valid input
+                        Validation.StringValidation(empAdd);
+                        //ask for the employers hourly wage
+                        Console.WriteLine("How much money do they make per hour?");
+                        //catch the response
+                        string empHour = Console.ReadLine();
+                        //validate the user entered a number
+                        Validation.IntegerValidation(empHour);
+                        //ask the user how many hours they work in a week
+                        Console.WriteLine("How many hours do they work a week?");
+                        //catch the response
+                        string empWeek = Console.ReadLine();
+                        //valid date the user entered a number
+                        Validation.IntegerValidation(empWeek);
+
+                        //convert the int variables to decimals
+                        decimal hpw = decimal.Parse(empWeek);
+                        decimal pph = decimal.Parse(empHour);
+
+                        //create the fulltime employee
+                        FullTime addFullTime = new FullTime(empName,empAdd,pph,hpw);
+                        employees.Add(addFullTime);
+
+                        //tell the user the employee has been added
+                        Console.WriteLine($"\r\n{employees[employees.Count-1].Name} has been added.");
+                        //allow the user to continue
+                        Console.WriteLine("\r\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+
+                
+                    case "2":
+                    case "parttime":
+                        Console.Clear();
+                        Console.WriteLine("case 2");
+                        break;
+                    case "3":
+                    case "salaried":
+                        Console.Clear();
+                        Console.WriteLine("case 3");
+                        break;
+                    case "4":
+                    case "manager":
+                        Console.WriteLine("case 4");
+                        break;
+
+                    default:
+                        Console.WriteLine("You did not enter a valid response. Please re-enter.");
+                        response = Console.ReadLine();
+                        break;
+
+                }
+
+                display = false;
+               
+                }
+
+            List<string> addList = new List<string>() { "Add Another Employee", "Return to the Main Menu" };
+            menu addEmployeeList = new menu(addList);
+
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+
+                //Display title
+                UI.ActionText("\r\nEmployee Tracker");
+                UI.Seperator();
+                //reset the color
+                Console.ResetColor();
+
+                //Instantiate
+                Console.WriteLine("\r\n");
+                addEmployeeList.DisplayMethod();
+                Console.WriteLine("\r\n");
+
+                //ask the user for the selection
+                Console.WriteLine("Please enter your selection:");
+                //catch the user repsonse
+                string options = Console.ReadLine();
+                //make sure the response is not blank
+                Validation.StringValidation(options);
+
+                int newOption = int.Parse(options);
+                Validation.Range((newOption), 2, 1);
+
+                //Begin switch statement that runs the option  the user selected
+                switch (newOption)
+
+                {
+                    case 1:
+                        Console.Clear();
+                        AddEmployee();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        keepGoing = false;
+                        break;
+
+
+                }
+            }
+
+
+
+            //Exit Message
+            Console.WriteLine("Have a nice day!");
+
+
+     }
 
 
         public void RemoveEmployee()
@@ -146,10 +296,7 @@ namespace Tykeeja_Harris_CE03
 
 
             //Display all the employees in the list
-            Console.WriteLine("\n{0,-20} {1,5}  {2, 20}\n", "Name", "Address", "Pay");
-            //calculate pay
-            decimal yearly = fulltime.CalculatePay();
-            Console.WriteLine(yearly);
+            Console.WriteLine("\n{0,-20} {1,5}  {2, 20}\n", "Name", "Address", "Pay");         
             //loop through the employee list to display the employee data
             for (int i =0; i < employees.Count; i++)
             {
